63565cb251e8c9c6acf2622344908caf4a221257
Huynh Duc Khoan <huynhduckhoan@gmail.com>
2020-04-29 14:15:25 +0700

    Update language box

--- app/components/RightSide/index.js
+++ app/components/RightSide/index.js
@@ -6,13 +6,48 @@
 
 import React, { memo } from 'react';
 import { FacebookProvider, Page } from 'react-facebook';
-import { RightWrapper } from './styled';
+import { Link } from 'react-router-dom';
+import { RightWrapper, LanguageBox } from './styled';
 // import PropTypes from 'prop-types';
-// import styled from 'styled-components';
 
 function RightSide() {
   return (
     <RightWrapper>
+      <LanguageBox>
+        <Link to="/en">
+          <img
+            className="language-icon"
+            typeof="foaf:Image"
+            src="https://dniit-i3s.cnrs.fr/sites/all/modules/languageicons/flags/en.png"
+            width="24"
+            height="18"
+            alt="English"
+            title="English"
+          />
+        </Link>
+        <Link to="/vi">
+          <img
+            className="language-icon"
+            typeof="foaf:Image"
+            src="https://dniit-i3s.cnrs.fr/sites/all/modules/languageicons/flags/vi.png"
+            width="24"
+            height="18"
+            alt="Tiếng Việt"
+            title="Tiếng Việt"
+          />
+        </Link>
+        <Link to="/fr">
+          <img
+            className="language-icon"
+            typeof="foaf:Image"
+            src="https://dniit-i3s.cnrs.fr/sites/all/modules/languageicons/flags/fr.png"
+            width="24"
+            height="18"
+            alt="Français"
+            title="Français"
+          />
+        </Link>
+      </LanguageBox>
       <FacebookProvider appId="365103820781953">
         <Page href="https://www.facebook.com/dniit.dn" tabs="timeline" />
       </FacebookProvider>
--- app/components/RightSide/styled.js
+++ app/components/RightSide/styled.js
@@ -4,3 +4,11 @@ export const RightWrapper = styled.div`
   width: 30%;
   display: inline-block;
 `;
+
+export const LanguageBox = styled.div`
+  padding: 10px;
+  display: flex;
+  a {
+    margin-right: 5px;
+  }
+`;
